eb5cc9d865c7d537b46f74cb1e902265a7719d84
Tong Chen <dimensionchan@gmail.com>
2016-04-24 13:54:57 -0700

    camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,27 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class camera : MonoBehaviour {
+public class CameraController : MonoBehaviour {
 	public GameObject gm;
 	// Use this for initialization
 	private GameObject fallenPlayer;
 	private float smooth;
 	private float size;
+	private GameManager script;
+
 	void Start () {
-		smooth = 1.0F;
+		smooth = 2.0F;
 		size = Camera.main.orthographicSize;
-	
+		script = gm.GetComponent<GameManager>();
+
 	}
 
 	// Update is called once per frame
 	void Update () {
 		Camera.main.orthographicSize = size;
-		GameManager script = gm.GetComponent<GameManager>();
+
 		if(script.isFallen){
 			fallenPlayer = script.fallenPlayer;	
 			//Camera.main.orthographicSize = Mathf.Lerp (size, 100.0F,Time.time);
-			Camera.main.orthographicSize = 300;
+			Camera.main.orthographicSize = 400;
 			transform.position =  Vector3.Lerp(transform.position,fallenPlayer.transform.position,Time.deltaTime*smooth );
+
+
 		}
 	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -36,6 +36,7 @@ public class GameManager : MonoBehaviour
 	//camera
 	public GameObject fallenPlayer ;
 	public bool isFallen = false;
+	public bool cameraDone = false;
 
     private static float pi = (float)Math.PI;
 
@@ -88,7 +89,7 @@ public class GameManager : MonoBehaviour
 
     }
 
-<<<<<<< HEAD
+
     public void addItemToList(ItemController item)
     {
         itemsOnBoard.Add(item);
@@ -164,7 +165,9 @@ public class GameManager : MonoBehaviour
 			isFallen = true;
             script.Opponent.GetComponent<PlayerController>().PlayerStop();
 			fallenPlayer = fallingPlayer;
-            script.triggerPlayerFall();
+
+
+			script.triggerPlayerFall();
 		
 
         }
@@ -231,146 +234,6 @@ public class GameManager : MonoBehaviour
     {
         PlayerPrefs.DeleteAll();
     }
-=======
-    public void addItemToList(ItemController item)
-    {
-        itemsOnBoard.Add(item);
-    }
+		
 
-    public void removeItemFromList(ItemController item)
-    {
-        itemsOnBoard.Remove(item);
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        /*
-        if (isPaused && EndPauseTime<Time.realtimeSinceStartup) {
-            Debug.Log("Resume");
-            ResumeGame();
-        }
-        */
-        if (isPaused || isGameOver)
-        {
-            return;
-        }
-        //generate items if not reach maximum
-        if (itemsOnBoard.Count < MaxItemsOnBoard)
-        {
-            nextSpawnTime -= Time.deltaTime;
-            if (nextSpawnTime <= 0)
-            {
-                float x = 0, y = 0, r = 0, colliderRadius = 0;
-                GameObject toInstantiate = null;
-                Vector3 instantiatePosition = new Vector3();
-                CircleCollider2D BackGroundCollider = GetComponent<CircleCollider2D>();
-                bool isValidPosition = false;
-                while (toInstantiate == null || !isValidPosition)
-                {
-                    nextSpawnTime = Random.Range(spawnInterval.minimum, spawnInterval.maximum);
-                    float theta = Random.Range(0f, 2f * pi);
-                    r = (float)Math.Sqrt(Random.Range(0f, 1f)) * radius;
-                    x = r * (float)Math.Cos(theta);
-                    y = r * (float)Math.Sin(theta);
-                    float p = Random.Range(0f, 1f);
-                    for (int i = PickUps.Length - 1; i >= 0; i--)
-                    {
-                        if (p >= ItemProbability[i])
-                        {
-                            toInstantiate = PickUps[i];
-                            break;
-                        }
-                    }
-                    colliderRadius = toInstantiate.transform.localScale.x * toInstantiate.GetComponent<CircleCollider2D>().radius;
-                    instantiatePosition = boardCenter + new Vector3(x, y, 0);
-
-                    Collider2D[] hits = Physics2D.OverlapCircleAll(instantiatePosition, colliderRadius);
-                    isValidPosition = hits.Length == 1 && hits[0] == BackGroundCollider;
-                }
-                Instantiate(toInstantiate, instantiatePosition, Quaternion.identity);
-            }
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D other)
-    {
-        //player off stage
-        if (!isPaused && (other.tag == "Player"))
-        {
-            isGameOver = true;
-            fallingPlayer = other.gameObject;
-            //focusCamera(fallingPlayer);
-            PlayerController script = other.GetComponent<PlayerController>();
-            script.PlayerStop();
-            script.Opponent.GetComponent<PlayerController>().PlayerStop();
-
-            script.triggerPlayerFall();
-        }
-    }
-
-    /*
-    public void focusCamera(GameObject player)
-    {
-        Camera.main.transform.localPosition = player.transform.localPosition + new Vector3(0, 0, -100);
-        Camera.main.fieldOfView += 1000;
-    }
-    */
-
-    public void JudgeGame()
-    {
-        fallingPlayer.SetActive(false);
-        String winner = fallingPlayer.name == "Blue" ? "Red" : "Blue";
-        int score = PlayerPrefs.GetInt(winner) + 1;
-        if (score >= WIN_SCORE)
-        {
-            PlayerPrefs.SetInt("Blue", 0);
-            PlayerPrefs.SetInt("Red", 0);
-            PlayerPrefs.SetString("Winner", winner);
-            SceneManager.LoadScene("Win");
-        }
-        else {
-            PlayerPrefs.SetInt(winner, score);
-            SceneManager.LoadScene("Main");
-        }
-    }
-
-    public void startNewGame()
-    {
-        SceneManager.LoadScene("Main");
-    }
-
-    public void PauseGame()
-    {
-        isPaused = true;
-        UIManager.instance.enbleButtons(false);
-        Time.timeScale = 0;
-    }
-
-    public void ResumeGame()
-    {
-        isPaused = false;
-        UIManager.instance.enbleButtons(true);
-        Time.timeScale = 1;
-    }
-
-
-    public IEnumerator PauseGameForSeconds(float pauseTime)
-    {
-        PauseGame();
-        float start = Time.realtimeSinceStartup;
-        while (Time.realtimeSinceStartup < start + pauseTime)
-        {
-            yield return null;
-        }
-        ResumeGame();
-    }
-
-
-    public void OnApplicationQuit()
-    {
-        PlayerPrefs.DeleteAll();
-    }
->>>>>>> master
 }
