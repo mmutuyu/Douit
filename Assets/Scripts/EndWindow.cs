a5de2687b7e96fb74bdd8f858d780c34541e73ad
mmutuyu <yuqianhome@gmail.com>
2016-04-25 08:52:00 -0700

    add start,pause, etc.

---
+++ Assets/Scripts/EndWindow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndWindow : GenericWindow {
+
+  public string winner = "Red";
+  public Image winnerImg;
+
+  // public void Start(){
+  //   if(winner.Equals("Red")){
+  //     Debug.Log("Red");
+  //   }else{
+  //     Debug.Log("Blue");
+  //   }
+  // }
+
+}
